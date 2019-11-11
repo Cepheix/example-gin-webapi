38d59c7c03acf434af2ba65bcb78d92cbc619815
Piotr <greenerenator@gmail.com>
2019-11-11 17:12:41 +0100

    Apply running migrations in main module

--- common/database.go
+++ common/database.go
@@ -1,17 +1,14 @@
 package common
 
 import (
-	"fmt"
-
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 func Init() *gorm.DB {
-	db, err := gorm.Open("sqlite3", "./../my-database.db")
-
+	db, err := gorm.Open("sqlite3", "my-database.db")
 	if err != nil {
-		fmt.Println("Cannot open database")
+		panic("failed to connect database")
 	}
 
 	return db
